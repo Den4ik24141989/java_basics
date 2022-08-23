fe9ee3c5e6bad0f5a156c2a54af14efd12a99220
denis_puzhinskii <425342Ab989>
2022-08-23 03:13:05 +0300

    Исправлено

--- ObjectsAndClasses_Methods/src/Arithmetic.java
+++ ObjectsAndClasses_Methods/src/Arithmetic.java
@@ -1,4 +1,12 @@
 public class Arithmetic {
+    public static void main(String[] args) {
+        Arithmetic arithmetic = new Arithmetic();
+        arithmetic.values(6,5);
+        System.out.println("Сумма значений: " + arithmetic.sum());
+        System.out.println("Произведение значений: " + arithmetic.multiplication());
+        System.out.println("Минимальное значение: " + arithmetic.min());
+        System.out.println("Максимальное значение: " + arithmetic.max());
+    }
     public int x;
     public int y;
 
